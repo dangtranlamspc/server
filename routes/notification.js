db7159685cfc746c64bc3b18d7d461846603cc6b
dangtranlamspc <tranlam@spchcmc.vn>
2025-09-10 11:35:04 +0700

    fix thong bao

--- routes/notification.js
+++ routes/notification.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const notificationController = require('../controllers/notification')
 
 router.get('/', notificationController.getNotification);
-router.put('/:id/read', notificationController.readNotification);
-router.put('/read-all', notificationController.allReadNotification);
+router.patch('/:id/read', notificationController.readNotification);
+router.patch('/read-all', notificationController.allReadNotification);
 router.delete('/:id', notificationController.deleteNotification);
 
 module.exports = router;
